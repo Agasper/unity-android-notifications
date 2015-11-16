7093dbeebd97eded76a734bc2f0286f678bb7e35
Alexander Agasper <agasper@yandex.ru>
2015-11-16 15:44:16 +0300

    Added support of Android M (API 23) for Test Project

--- UnityProject/Assets/LocalNotification.cs
+++ UnityProject/Assets/LocalNotification.cs
@@ -1,21 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 class LocalNotification
 {
+    /// <summary>
+    /// Inexact uses `set` method
+    /// Exact uses `setExact` method
+    /// ExactAndAllowWhileIdle uses `setAndAllowWhileIdle` method
+    /// Documentation: https://developer.android.com/intl/ru/reference/android/app/AlarmManager.html
+    /// </summary>
+    public enum NotificationExecuteMode
+    {
+        Inexact = 0,
+        Exact = 1,
+        ExactAndAllowWhileIdle = 2
+    }
+
 #if UNITY_ANDROID && !UNITY_EDITOR
     private static string fullClassName = "net.agasper.unitynotification.UnityNotificationManager";
     private static string unityClass = "com.unity3d.player.UnityPlayerNativeActivity";
 #endif
+
+    public static void SendNotification(int id, TimeSpan delay, string title, string message, string icon_name)
+    {
+        SendNotification(id, (int)delay.TotalSeconds, title, message, Color.white);
+    }
     
-    public static void SendNotification(int id, long delay, string title, string message, Color32 bgColor, bool sound = true, bool vibrate = true, bool lights = true, string bigIcon = "")
+    public static void SendNotification(int id, long delay, string title, string message, Color32 bgColor, bool sound = true, bool vibrate = true, bool lights = true, string bigIcon = "", NotificationExecuteMode executeMode = NotificationExecuteMode.Inexact)
     {
 #if UNITY_ANDROID && !UNITY_EDITOR
         AndroidJavaClass pluginClass = new AndroidJavaClass(fullClassName);
         if (pluginClass != null)
         {
-            pluginClass.CallStatic("SetNotification", id, unityClass, delay * 1000L, title, message, message, sound ? 1 : 0, vibrate ? 1 : 0, lights ? 1 : 0, bigIcon, "notify_icon_small", bgColor.r * 65536 + bgColor.g * 256 + bgColor.b);
+            pluginClass.CallStatic("SetNotification", id, unityClass, delay * 1000L, title, message, message, sound ? 1 : 0, vibrate ? 1 : 0, lights ? 1 : 0, bigIcon, "notify_icon_small", bgColor.r * 65536 + bgColor.g * 256 + bgColor.b, (int)executeMode);
         }
 #endif
     }
--- UnityProject/Assets/NotificationTest.cs
+++ UnityProject/Assets/NotificationTest.cs
@@ -3,20 +3,42 @@ using System.Collections;
 
 public class NotificationTest : MonoBehaviour {
 
+    float sleepUntil = 0;
 	
 	void OnGUI () {
         //Color is supported only in Android >= 5.0
+        GUI.enabled = sleepUntil < Time.time;
 
-		if (GUILayout.Button("5 SECONDS", GUILayout.Height(Screen.height * 0.2f)))
-			LocalNotification.SendNotification(1, 5, "Title", "Long message text", new Color32(0xff, 0x44, 0x44, 255));
+        if (GUILayout.Button("5 SECONDS", GUILayout.Height(Screen.height * 0.2f)))
+        {
+            LocalNotification.SendNotification(1, 5, "Title", "Long message text", new Color32(0xff, 0x44, 0x44, 255));
+            sleepUntil = Time.time + 5;
+        }
 
         if (GUILayout.Button("5 SECONDS BIG ICON", GUILayout.Height(Screen.height * 0.2f)))
-            LocalNotification.SendNotification(2, 5, "Title", "Long message text with big icon", new Color32(0xff, 0x44, 0x44, 255), true, true, true, "app_icon");
+        {
+            LocalNotification.SendNotification(1, 5, "Title", "Long message text with big icon", new Color32(0xff, 0x44, 0x44, 255), true, true, true, "app_icon");
+            sleepUntil = Time.time + 5;
+        }
 
         if (GUILayout.Button("EVERY 5 SECONDS", GUILayout.Height(Screen.height * 0.2f)))
-			LocalNotification.SendRepeatingNotification(3, 5, 5, "Title", "Long message text", new Color32(0xff, 0x44, 0x44, 255));
-			
-		if (GUILayout.Button("STOP",GUILayout.Height(Screen.height * 0.2f)))
-			LocalNotification.CancelNotification(1);
+        {
+            LocalNotification.SendRepeatingNotification(1, 5, 5, "Title", "Long message text", new Color32(0xff, 0x44, 0x44, 255));
+            sleepUntil = Time.time + 99999;
+        }
+
+        if (GUILayout.Button("10 SECONDS EXACT", GUILayout.Height(Screen.height * 0.2f)))
+        {
+            LocalNotification.SendNotification(1, 10, "Title", "Long exact message text", new Color32(0xff, 0x44, 0x44, 255), executeMode: LocalNotification.NotificationExecuteMode.ExactAndAllowWhileIdle);
+            sleepUntil = Time.time + 10;
+        }
+
+        GUI.enabled = true;
+
+        if (GUILayout.Button("STOP", GUILayout.Height(Screen.height * 0.2f)))
+        {
+            LocalNotification.CancelNotification(1);
+            sleepUntil = 0;
+        }
 	}
 }
